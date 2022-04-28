ac7001a564661819b44c4817863cee3d6364355d
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-04-28 17:09:34 +0200

    Liste d'éléments dans la page de recherche

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
 
+export interface SearchResult {
+  city: string;
+  zipcode: number;
+}
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss'],
 })
-export class SearchPage {}
+export class SearchPage {
+  searchResults: SearchResult[] = [
+    {
+      city: 'Paris',
+      zipcode: 75020,
+    },
+    {
+      city: 'Nantes',
+      zipcode: 21000,
+    },
+    {
+      city: 'Marseille',
+      zipcode: 13000,
+    },
+  ];
+}
