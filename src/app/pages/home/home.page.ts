b2404478e24a72b99a66d6462ca1b20489b5d8b6
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-04-28 16:33:25 +0200

    Ajout de la searchbar clic sur le bouton de recherche pour l'afficher

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage {}
+export class HomePage {
+  isSearchBarVisible = false;
+
+  showSearchBar() {
+    this.isSearchBarVisible = !this.isSearchBarVisible;
+  }
+}
