54bb708f7ae60c1c4bd465e51515882ee18cbc98
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-04-28 17:44:27 +0200

    Installation du ngx translate

--- src/app/pages/search/search.module.ts
+++ src/app/pages/search/search.module.ts
@@ -5,12 +5,20 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { ComponentsModule } from 'src/app/components/components.module';
+import { TranslateModule } from '@ngx-translate/core';
 import { SearchPageRoutingModule } from './search-routing.module';
 
 import { SearchPage } from './search.page';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, SearchPageRoutingModule, ComponentsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    SearchPageRoutingModule,
+    ComponentsModule,
+    TranslateModule.forChild(),
+  ],
   declarations: [SearchPage],
 })
 export class SearchPageModule {}
