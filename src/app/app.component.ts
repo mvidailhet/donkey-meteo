6335628926d5813da54728309f9d406f18f7adb4
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-04-30 23:05:28 +0200

    désactivation du coverage, car il n'est pas utilisé pour le moment fix de l'erreur Jest pour passer la PR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'error';
+  title = 'donkey-meteo';
   constructor(translate: TranslateService) {
     translate.use('fr');
   }
