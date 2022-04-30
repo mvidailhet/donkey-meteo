6d8c73eaf1b3be326e295b91a193037505e47745
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-04-30 21:14:29 +0200

    fix jest ionic error

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateTestingModule } from 'ngx-translate-testing';
+import { IonicModule } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import * as frenchTranslation from '../assets/i18n/fr.json';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, TranslateTestingModule.withTranslations('fr', frenchTranslation)],
+      imports: [
+        RouterTestingModule,
+        IonicModule.forRoot(),
+        TranslateTestingModule.withTranslations('fr', frenchTranslation),
+      ],
       declarations: [AppComponent],
     }).compileComponents();
   });
