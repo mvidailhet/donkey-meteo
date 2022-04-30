096580da4578ce9f5184a0243e6076631a83aa80
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-04-30 19:34:07 +0200

    Jest is now working for 2 spec files

--- src/app/pages/search/search.page.spec.ts
+++ src/app/pages/search/search.page.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { TranslateTestingModule } from 'ngx-translate-testing';
+import { ComponentsModule } from '../../components/components.module';
+import * as frenchTranslation from '../../../assets/i18n/fr.json';
 import { SearchPage } from './search.page';
 
 describe('SearchPage', () => {
@@ -10,7 +13,12 @@ describe('SearchPage', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [SearchPage],
-      imports: [IonicModule.forRoot()],
+      imports: [
+        RouterTestingModule,
+        TranslateTestingModule.withTranslations(frenchTranslation),
+        IonicModule.forRoot(),
+        ComponentsModule,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SearchPage);
