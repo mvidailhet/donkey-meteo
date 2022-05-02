216ce9dfa411f4ae5d27dba4ebb54ace21c10a9a
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-05-02 14:00:00 +0200

    suppression du code mort

--- src/app/services/google/places.service.ts
+++ src/app/services/google/places.service.ts
@@ -1,4 +1,4 @@
-import { ElementRef, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
 @Injectable({
@@ -18,55 +18,5 @@ export class PlacesService {
       this.placeSubject.next(place);
       console.log('place :>> ', place);
     });
-  }
-
-  getAddrComponent(place: any, componentTemplate: any): any {
-    let result;
-
-    place.address_components.forEach((component: any) => {
-      const addressType = component.types[0];
-      if (componentTemplate[addressType]) {
-        result = component[componentTemplate[addressType]];
-        return result;
-      }
-      return null;
-    });
-  }
-
-  getStreetNumber(place: any): any {
-    const COMPONENT_TEMPLATE = { street_number: 'short_name' };
-    return this.getAddrComponent(place, COMPONENT_TEMPLATE);
-  }
-  getStreet(place: any): any {
-    const COMPONENT_TEMPLATE = { route: 'long_name' };
-    return this.getAddrComponent(place, COMPONENT_TEMPLATE);
-  }
-  getCity(place: any): any {
-    const COMPONENT_TEMPLATE = { locality: 'long_name' };
-    return this.getAddrComponent(place, COMPONENT_TEMPLATE);
-  }
-  getState(place: any): any {
-    const COMPONENT_TEMPLATE = { administrative_area_level_1: 'short_name' };
-    return this.getAddrComponent(place, COMPONENT_TEMPLATE);
-  }
-  getDistrict(place: any): any {
-    const COMPONENT_TEMPLATE = { administrative_area_level_2: 'short_name' };
-    return this.getAddrComponent(place, COMPONENT_TEMPLATE);
-  }
-  getCountryShort(place: any): any {
-    const COMPONENT_TEMPLATE = { country: 'short_name' };
-    return this.getAddrComponent(place, COMPONENT_TEMPLATE);
-  }
-  getCountry(place: any): any {
-    const COMPONENT_TEMPLATE = { country: 'long_name' };
-    return this.getAddrComponent(place, COMPONENT_TEMPLATE);
-  }
-  getPostCode(place: any): any {
-    const COMPONENT_TEMPLATE = { postal_code: 'long_name' };
-    return this.getAddrComponent(place, COMPONENT_TEMPLATE);
-  }
-  getPhone(place: any): any {
-    const COMPONENT_TEMPLATE = { formatted_phone_number: 'formatted_phone_number' };
-    return this.getAddrComponent(place, COMPONENT_TEMPLATE);
   }
 }
