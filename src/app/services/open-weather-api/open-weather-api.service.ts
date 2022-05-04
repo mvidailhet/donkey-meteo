7f9cfd7599997fb029c9c3aee2a972739f7e4e80
Nelson Belgarde <nelsonbelgarde@gmail.com>
2022-05-04 22:48:43 +0200

    Ajout des clés dans 'WeatherIconEnum' corespondant au code icon de la nuit enovyé par l'api

--- src/app/services/open-weather-api/open-weather-api.service.ts
+++ src/app/services/open-weather-api/open-weather-api.service.ts
@@ -3,13 +3,21 @@ import { Injectable } from '@angular/core';
 
 export enum WeatherIconEnum {
   '02d' = 'cloudy-day-3',
+  '02n' = 'cloudy-night-3',
   '03d' = 'cloudy',
+  '03n' = 'cloudy',
   '04d' = 'cloudy',
+  '04n' = 'cloudy',
   '01d' = 'day',
+  '01n' = 'night',
   '10d' = 'rainy-3',
+  '10n' = 'rainy-3',
   '09d' = 'rainy-7',
+  '09n' = 'rainy-7',
   '13d' = 'snowy-6',
+  '13n' = 'snowy-6',
   '11d' = 'thunder',
+  '11n' = 'thunder',
 }
 
 @Injectable({
