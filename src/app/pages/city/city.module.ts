e73bd07ddd9880faaa5c5324bb48d36561c2bd98
Nelson Belgarde <nelsonbelgarde@gmail.com>
2022-05-04 23:52:00 +0200

    Création des onglets pour les prévisions à la quinzaine ou au jour

--- src/app/pages/city/city.module.ts
+++ src/app/pages/city/city.module.ts
@@ -4,12 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
+import { TranslateModule } from '@ngx-translate/core';
 import { CityPageRoutingModule } from './city-routing.module';
 
 import { CityPage } from './city.page';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, CityPageRoutingModule],
+  imports: [CommonModule, FormsModule, IonicModule, CityPageRoutingModule, TranslateModule.forChild()],
   declarations: [CityPage],
 })
 export class CityPageModule {}
